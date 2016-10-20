54a8abff62d5da5690177550fb0b7b29683b979f
ponyspy <desade4me@gmail.com>
2016-10-20 21:44:14 +0300

    pars

--- parse.js
+++ parse.js
@@ -10,10 +10,37 @@ jsdom.env({
 	done: function (err, window) {
 		var $ = window.$;
 
-		// Parse here
-		var result = [];
+		var result = $('tr').slice(1).map(function() {
+			var td = $(this).children('td');
 
-		fs.writeFile('./out/result.json', JSON.stringify(result, null, 2), 'utf8', function(err) {
+			return {
+				hall: +td.eq(15).text().replace(/\t+/g, '').replace(/\n/g, ' ').trim().replace('Павильон № ', ''),
+				file_name: td.eq(6).text().replace(/\t+/g, '').replace(/\n/g, ' ').trim(),
+
+				ru: {
+					title: td.eq(0).text().replace(/\t+/g, '').replace(/\n/g, ' ').trim(),
+					description: td.eq(1).text().replace(/\t+/g, '').replace(/\n/g, ' ').trim(),
+					description_alt: td.eq(2).text().replace(/\t+/g, '').replace(/\n/g, ' ').trim(),
+					place_year: td.eq(3).text().replace(/\t+/g, '').replace(/\n/g, ' ').trim(),
+					museum: td.eq(4).text().replace(/\t+/g, '').replace(/\n/g, ' ').trim(),
+					size_mat: td.eq(5).text().replace(/\t+/g, '').replace(/\n/g, ' ').trim(),
+					complex: td.eq(7).text().replace(/\t+/g, '').replace(/\n/g, ' ').trim(),
+				},
+
+				en: {
+					title: td.eq(8).text().replace(/\t+/g, '').replace(/\n/g, ' ').trim(),
+					description: td.eq(9).text().replace(/\t+/g, '').replace(/\n/g, ' ').trim(),
+					description_alt: td.eq(10).text().replace(/\t+/g, '').replace(/\n/g, ' ').trim(),
+					place_year: td.eq(11).text().replace(/\t+/g, '').replace(/\n/g, ' ').trim(),
+					museum: td.eq(12).text().replace(/\t+/g, '').replace(/\n/g, ' ').trim(),
+					size_mat: td.eq(13).text().replace(/\t+/g, '').replace(/\n/g, ' ').trim(),
+					complex: td.eq(14).text().replace(/\t+/g, '').replace(/\n/g, ' ').trim(),
+				}
+
+			}
+		}).toArray();
+
+		fs.writeFile('./data/raw/result.json', JSON.stringify(result, null, 2), 'utf8', function(err) {
 			if (err) throw err;
 
 			console.log('json saved');
